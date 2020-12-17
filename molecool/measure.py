f0e30b32a2127ab0972206e93f62eba0ee7a7ae8
Ryan Posh <rposh@nd.edu>
2020-12-17 14:01:03 -0500

    Added docstring to measure.py

--- molecool/measure.py
+++ molecool/measure.py
@@ -15,6 +15,29 @@ def calculate_angle(rA, rB, rC, degrees=False):
         return theta
 
 def calculate_distance(rA, rB):
+    """
+    Calculates the distance between two points.
+    
+    Parameters
+    ----------
+    rA : np.array
+        The coordinates of point A.
+    rB : np.array
+        The coordinates of point B.
+
+    Returns
+    -------
+    dist : float
+        The distance between rA and rB.
+        
+    Examples
+    --------
+    >>> r1 = np.array([0,0,0])
+    >>> r2 = np.array([0,1,0])
+    >>> calculate_distance(r1,r2)
+    1.0
+    """
+    
     d=(rA-rB)
     dist=np.linalg.norm(d)
     return dist
