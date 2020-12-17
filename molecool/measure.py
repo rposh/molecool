7c06963e66da8780ee43689985686207b09de6ab
Ryan Posh <rposh@nd.edu>
2020-12-17 14:15:54 -0500

    Ran black style on modules

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,20 +4,22 @@ This module is for functions which perform measurements.
 
 import numpy as np
 
+
 def calculate_angle(rA, rB, rC, degrees=False):
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
 
     if degrees:
         return np.degrees(theta)
     else:
         return theta
 
+
 def calculate_distance(rA, rB):
     """
     Calculates the distance between two points.
-    
+
     Parameters
     ----------
     rA : np.array
@@ -29,7 +31,7 @@ def calculate_distance(rA, rB):
     -------
     dist : float
         The distance between rA and rB.
-        
+
     Examples
     --------
     >>> r1 = np.array([0,0,0])
@@ -37,7 +39,7 @@ def calculate_distance(rA, rB):
     >>> calculate_distance(r1,r2)
     1.0
     """
-    
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
+
+    d = rA - rB
+    dist = np.linalg.norm(d)
     return dist
